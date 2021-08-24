b5d6ee4db34ddb2f37e34afb218b7f66be1ffa35
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-08-24 23:51:00 +0530

    Attempt to grade

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const query = gql`
   }
 `;
 
-const endpoint = "https://www.pupilfirst.school/graphql";
+const endpoint = "https://vta-test-school-7146.pflms.net/graphql";
 
 const graphQLClient = new GraphQLClient(endpoint, {
   headers: {
@@ -19,10 +19,42 @@ const graphQLClient = new GraphQLClient(endpoint, {
   },
 });
 
+const mutation = gql`
+  mutation GradeSubmission(
+    $submissionId: ID!
+    $grades: [GradeInput!]!
+    $checklist: JSON!
+    $feedback: String
+  ) {
+    createGrading(
+      submissionId: $submissionId
+      grades: $grades
+      checklist: $checklist
+      feedback: $feedback
+    ) {
+      success
+    }
+  }
+`;
+
+const variables = {
+  submissionId: "246279",
+  grades: [{ evaluationCriterionId: "2695", grade: 2 }],
+  checklist: [
+    {
+      kind: "shortText",
+      title: "Describe your submission",
+      result: "This is something awesome",
+      status: "noAnswer",
+    },
+  ],
+  feedback: "Thats great job",
+};
+
 // most @actions toolkit packages have async methods
 async function run() {
-  const data = await graphQLClient.request(query);
+  const data = await graphQLClient.request(mutation, variables);
   console.log(JSON.stringify(data, undefined, 2));
 }
 
-run();
+run().catch((error) => console.log(error));
